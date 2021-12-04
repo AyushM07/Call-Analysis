2f4f112ffa4ed0f326b40d5fde4c04fc5097a49c
AyushM07 <84060230+AyushM07@users.noreply.github.com>
2021-12-04 18:48:47 -0500

    Filtered df according to zip codes
    Zip code 10027

--- Top 10.py
+++ Top 10.py
@@ -1,4 +1,5 @@
 import csv 
 import pandas as pd 
-df= pd.read_csv('311_Service_Requests_2020')
+df= pd.read_csv('/Users/AyushMadhogaria/Desktop/311_Service_Requests_2020.csv')
+df=df.loc[df['Incident Zip']==10027]
 df
