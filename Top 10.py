74e398e72cf92bd1087190885c196e272ecc7c19
AyushM07 <84060230+AyushM07@users.noreply.github.com>
2021-12-04 20:13:25 -0500

    Found top 10
    Created a pandas series called top10 showing top 10 causes of calls to 311 in the zip code: 10027 in the year 2020, in descending order. 
    The pandas series contains the cause of call and the number of times calls with this cause have been made.

--- Top 10.py
+++ Top 10.py
@@ -2,4 +2,6 @@ import csv
 import pandas as pd 
 df= pd.read_csv('/Users/AyushMadhogaria/Desktop/311_Service_Requests_2020.csv')
 df=df.loc[df['Incident Zip']==10027]
-df
+a= df['Descriptor'].value_counts()
+top10= a.nlargest(10)
+top10
